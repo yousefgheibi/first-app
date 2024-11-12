91af94f2e7a969fd68d54ddf40b5e56a39472ab1
Yousef Gheibi <gheybi@optt.ca>
2024-11-12 19:12:12 +0330

    use images

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,13 @@
 import styled from './article.module.css';
+import article from './../../assets/images/article.png'
 
 function Article(){
 
     return (
         <div className={styled.articleWrapper}>
-            <p>مقاله</p>
+            <img src={article} />
+            <h3>متغییر ها در جاوااسکریپت</h3>
+            <p>خواندن در 5 دقیقه</p>
         </div>
     )
 }
