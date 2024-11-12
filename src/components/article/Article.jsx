3ea5ffce76abb1e20999969b4703233c9ad88fb3
Yousef Gheibi <gheybi@optt.ca>
2024-11-12 20:44:16 +0330

    get data from json

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,16 +1,13 @@
 import styled from './article.module.css';
-import article from './../../assets/images/article.png'
 
-function Article(){
-
+function Article(props){
     return (
         <div className={styled.articleWrapper}>
-            <img src={article} />
-            <h3>متغییر ها در جاوااسکریپت</h3>
-            <p>خواندن در 5 دقیقه</p>
+            <img src={props.imageUrl} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>خواندن در  {props.readingTime} دقیقه</p>
         </div>
     )
 }
-
 
 export default Article;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import Article from "../../components/article/Article";
 import Navbar from "../../components/navbar/Navbar";
 import styled from "./home.module.css";
+import axios from "axios";
 
 function Home() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/articles').then(result => {
+            setArticles(result.data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }, []);
 
     return (
         <div>
@@ -10,18 +21,21 @@ function Home() {
             <div className="container">
                 <div className={styled.homeWrapper}>
                     <h2>مقالات جدید</h2>
-
                     <div className={styled.articles}>
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
+                        {articles.map((article) => (
+                            <Article
+                                key={article.id}
+                                id={article.id}
+                                imageUrl={article.imageUrl}
+                                title={article.title}
+                                readingTime={article.readingTime}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
