c3e19a832c5bc0decf99191fcdf2030092a05e1d
Yousef Gheibi <gheybi@optt.ca>
2024-11-14 17:40:12 +0330

    input onChange

--- src/pages/createArticle/CreateArticle.jsx
+++ src/pages/createArticle/CreateArticle.jsx
@@ -2,28 +2,25 @@ import { Fragment, useState } from "react";
 import styled from "./create-article.module.css";
 
 function CreateArticle() {
-    const [counter, setCounter] = useState(0);
 
-    const increaseCounter = () => {
-        setCounter(counter+1)
+    const [articleTitle , setArticleTitle] = useState("");
+
+    const handleChangeArticle = (event) =>{
+        setArticleTitle(event.target.value)
     }
 
-    const decreaseCounter = () => {
-        setCounter(counter-1)
-    }
-
+    console.log('aritlceTitle', articleTitle);
     return (
         <Fragment>
-            <div className="container">
-
-                <div className={styled.wrapper}>
+            <div className={styled.wrapper}>
+                <div className="container">
                     <h2>ساخت مقاله</h2>
-
-
-                    <button onClick={increaseCounter}>افزایش</button>
-                    {counter}
-                    <button onClick={decreaseCounter}>کاهش</button>
+                <div className={styled.inputWrapper}>
+                    <label>عنوان</label>
+                    <input onChange={handleChangeArticle} dir="auto" type="text" />
                 </div>
+                </div>
+
             </div>
         </Fragment>
     )
