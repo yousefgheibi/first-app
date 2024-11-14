926c8b37063155b867110890770e8f66696a2778
Yousef Gheibi <gheybi@optt.ca>
2024-11-14 12:37:04 +0330

    get article detail data

--- src/pages/articleDtails/ArticleDetails.jsx
+++ src/pages/articleDtails/ArticleDetails.jsx
@@ -1,14 +1,40 @@
 import { useParams } from "react-router-dom";
 import styled from "./article-details.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 function ArticleDetails() {
 
+    const [article, setArticle] = useState();
     const params = useParams();
-    console.log(params)
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/articles/${params.id}`).then((res) => {
+            setArticle(res.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
     return (
-        <div>
-            <h2>Article Details</h2>
+        <div className="container">
+            <div class={styled.wrapper}>
+                {article ? (
+                    <div>
+                        <h3>{article.title}</h3>
+                        <div class={styled.summery}>
+                            <p>نویسنده : {article.writer}</p>
+                            <p>تاریخ ایجاد : {article.date}</p>
+                            <p>مدت زمان خواندن : {article.readingTime} دقیقه</p>
+                        </div>
+                        <img src={article.imageUrl} alt={article.title} />
+                        <p>{article.content}</p>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
     )
 }
