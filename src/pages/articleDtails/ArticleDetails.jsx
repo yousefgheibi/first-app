5188b7cf8321c81f64b225785bc568201d00cafb
Yousef Gheibi <gheybi@optt.ca>
2024-11-14 12:12:59 +0330

    get params with useParams hook

--- src/pages/articleDtails/ArticleDetails.jsx
+++ src/pages/articleDtails/ArticleDetails.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom";
 import styled from "./article-details.module.css";
 
+
 function ArticleDetails() {
+
+    const params = useParams();
+    console.log(params)
     return (
         <div>
             <h2>Article Details</h2>
