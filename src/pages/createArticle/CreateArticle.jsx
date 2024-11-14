3d7678d32959874d3f7a7273656bfac7992870ec
Yousef Gheibi <gheybi@optt.ca>
2024-11-14 18:18:06 +0330

    get all input value

--- src/pages/createArticle/CreateArticle.jsx
+++ src/pages/createArticle/CreateArticle.jsx
@@ -3,22 +3,40 @@ import styled from "./create-article.module.css";
 
 function CreateArticle() {
 
-    const [articleTitle , setArticleTitle] = useState("");
-
-    const handleChangeArticle = (event) =>{
-        setArticleTitle(event.target.value)
+    const [article, setArticle] = useState({
+        title: "",
+        date: ""
+    });
+   
+    const handleChangeArticle = (event) => {
+        switch (event.target.name) {
+            case "title":
+                setArticle({
+                    title:event.target.value
+                })
+                break;
+            case "date":
+                setArticle({
+                    date:event.target.value
+                })
+                break;
+        }
     }
 
-    console.log('aritlceTitle', articleTitle);
+    console.log(article)
     return (
         <Fragment>
             <div className={styled.wrapper}>
                 <div className="container">
                     <h2>ساخت مقاله</h2>
-                <div className={styled.inputWrapper}>
-                    <label>عنوان</label>
-                    <input onChange={handleChangeArticle} dir="auto" type="text" />
-                </div>
+                    <div className={styled.inputWrapper}>
+                        <label>عنوان</label>
+                        <input name="title" onChange={handleChangeArticle} dir="auto" type="text" />
+                    </div>
+                    <div className={styled.inputWrapper}>
+                        <label>تاریخ</label>
+                        <input name="date" onChange={handleChangeArticle} dir="auto" type="text" />
+                    </div>
                 </div>
 
             </div>
